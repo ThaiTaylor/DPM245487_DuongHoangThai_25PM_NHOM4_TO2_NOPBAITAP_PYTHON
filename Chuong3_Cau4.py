326044f8490303e7c63fcedb814218c4e7bdc5a1
dhthai <duonghoangthai281006@gmail.com>
2025-09-27 14:06:51 +0700

    27/09

---
+++ Chuong3_Cau4.py
@@ -0,0 +1,18 @@
+#Câu 4: Hãy cho biết kết quả của Boolean Expression
+'''
+Yêu cầu:
+Cho x, y, z = 3, 5, 7. Hãy cho biết kết quả của Boolean Expression
+
+(a) x == 3  --> TRUE
+(b) x < y   --> TRUE
+(c) x >= y  --> False
+(d) x <= y  --> TRUE
+(e) x != y - 2  -->False
+(f) x < 10  --> TRUE
+(g) x >= 0 and x < 10   -->TRUE
+(h) x < 0 and x < 10    --> False
+(i) x >= 0 and x < 2    --> False
+(j) x < 0 or x < 10     --> TRUE
+(k) x > 0 or x < 10     --> TRUE   
+(l) x < 0 or x > 10     --> False
+'''
