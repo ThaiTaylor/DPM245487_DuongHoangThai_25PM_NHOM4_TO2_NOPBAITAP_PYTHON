a693ba309c7f388962adbd3afea9a12e4aafdb48
dhthai <duonghoangthai281006@gmail.com>
2025-10-01 19:37:19 +0700

    1/10

---
+++ Chuong3_Cau16.py
@@ -0,0 +1,11 @@
+#Câu 16: Cho biết bao nhiêu dấu * được in ra trên màn hình
+'''
+Yêu cầu:
+Cho biết bao nhiêu dấu * được in ra trên màn hình
+for a in range(20, 100, 5): 
+    print('*', end='') 
+print()
+
+Trả lời
+Có 16 dấu * được in ra.
+'''
