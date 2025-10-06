2fbd772c51a1ce645d0bfad672082736b683a040
dhthai <duonghoangthai281006@gmail.com>
2025-10-06 21:35:19 +0700

    6/10

---
+++ Chuong4_Cau12.py
@@ -0,0 +1,18 @@
+#Câu 12: Hàm oscillate
+'''
+Yêu cầu:
+Cho mã lệnh:
+for n in oscillate(-3, 5):
+    print(n, end=' ')
+print()
+Hãy viết hàm oscillate để khi chạy phần mềm, nó ra kết quả:
+-3 3 -2 2 -1 1 0 0 1 -1 2 -2 3 -3 4 -4
+'''
+def oscillate(start, end):
+    for i in range(start, end):
+        yield i
+        yield -i
+
+for n in oscillate(-3, 5):
+    print(n, end=' ')
+print()
