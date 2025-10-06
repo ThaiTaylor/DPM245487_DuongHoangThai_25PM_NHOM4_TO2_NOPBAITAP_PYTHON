6060d273b665ebe6c2e197b72dce43c928fea1c1
dhthai <duonghoangthai281006@gmail.com>
2025-10-06 21:27:26 +0700

    6/10

---
+++ Chuong4_Cau10.py
@@ -0,0 +1,56 @@
+#Câu 10: Vẽ hình dùng Sleep
+
+import time
+
+# Hình 1
+print("""
+      *
+      * * 
+      * * *
+* * * * * * *
+* * *
+* *
+*
+""")
+
+# Dừng 5 giây
+time.sleep(5)
+
+# Hình 2
+print("""
+      *
+      * * 
+      *   *
+* * * * * * *
+*   *
+* *
+*
+""")
+
+# Dừng 5 giây
+time.sleep(5)
+
+# Hình 3
+print("""
+      * * * *
+      * * *
+      * * 
+      *
+    * *
+  * * *
+* * * *
+""")
+
+# Dừng 5 giây
+time.sleep(5)
+
+# Hình 4
+print("""
+      * * * *
+      *   *
+      * * 
+      *
+    * *
+  *   *
+* * * *
+""")
