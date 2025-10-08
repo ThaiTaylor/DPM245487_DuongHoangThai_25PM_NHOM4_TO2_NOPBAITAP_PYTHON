9a6cdaf22e3bc6acc534d8be2a47b025eee526ae
dhthai <duonghoangthai281006@gmail.com>
2025-10-08 18:59:44 +0700

    8/10

---
+++ Chuong5_Cau4.py
@@ -0,0 +1,13 @@
+#Câu 4: Các hàm quan trọng trong xử lý chuỗi của Python
+'''
+Yêu cầu:
+Trình bày một số hàm quan trọng trong xử lý Chuỗi của Python
+
+Trả lời
+Kiểm tra	            isalpha(), isdigit(), isupper()
+Chuyển đổi chữ	        lower(), upper(), title()
+Xóa khoảng trắng	    strip(), lstrip(), rstrip()
+Tìm kiếm / Thay thế	    find(), replace(), count()
+Tách / Ghép	            split(), join()
+Định dạng	            format(), f-string
+'''
