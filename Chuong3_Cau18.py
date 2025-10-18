6ff3b8b099a20e56d194a58ee13abe70ade0b512
dhthai <duonghoangthai281006@gmail.com>
2025-10-18 11:00:02 +0700

    8/10 fix

--- Chuong3_Cau18.py
+++ Chuong3_Cau18.py
@@ -25,7 +25,7 @@ n = int(input("Nhập n: "))
 
 for i in range(n):
     for j in range(n):
-        if j == 0 or j == i or i == n-1:   # cạnh trái, cạnh chéo, cạnh đáy
+        if j == 0 or j == i or i == n-1:   
             print("*", end=" ")
         else:
             print(" ", end=" ")
