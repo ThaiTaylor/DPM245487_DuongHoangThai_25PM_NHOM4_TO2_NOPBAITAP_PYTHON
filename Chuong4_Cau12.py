1c57852f9ad8ebd3d38195037d2b903878451ea9
dhthai <duonghoangthai281006@gmail.com>
2025-10-19 20:36:58 +0700

    19/10 fix

--- Chuong4_Cau12.py
+++ Chuong4_Cau12.py
@@ -8,6 +8,7 @@ print()
 Hãy viết hàm oscillate để khi chạy phần mềm, nó ra kết quả:
 -3 3 -2 2 -1 1 0 0 1 -1 2 -2 3 -3 4 -4
 '''
+
 def oscillate(start, end):
     for i in range(start, end):
         yield i
