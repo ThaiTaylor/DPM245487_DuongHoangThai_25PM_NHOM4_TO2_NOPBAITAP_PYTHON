e12e02504149fc03ca8bda4f4ee234df421e1937
dhthai <duonghoangthai281006@gmail.com>
2025-10-22 19:56:29 +0700

    22/10

---
+++ Chuong6_Cau4.py
@@ -0,0 +1,27 @@
+#Câu 4: Xác định kết quả khi thực thi list
+'''
+Yêu cầu:
+Cho list
+lst = [3, 0, 1, 5, 2]
+x = 2
+Hãy cho biết kết quả:
+(a) lst[0]?
+(b) lst[3]?
+(c) lst[x]?
+(d) lst[-x]?
+(e) lst[x + 1]?
+(f) lst[x] + 1?
+(g) lst[lst[x]]?
+(h) lst[lst[lst[x]]]?
+
+
+KẾT QUẢ 
+(a) 3
+(b) 5
+(c) 1
+(d) 5
+(e) 5
+(f) 2
+(g) 0
+(h) 3
+'''
