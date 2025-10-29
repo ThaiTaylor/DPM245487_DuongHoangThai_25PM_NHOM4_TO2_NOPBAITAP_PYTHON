bf3eb845c1c9082da9f439dcc9401d53fb9beaba
dhthai <duonghoangthai281006@gmail.com>
2025-10-29 18:31:04 +0700

    29/10

--- Chuong7_Cau5.py
+++ Chuong7_Cau5.py
@@ -10,22 +10,21 @@ pythonObject = {
 Hãy viết mã lệnh chuyển đổi qua String json.
 '''
 
-# ==============================
+
 # Câu 5: Xử lý JSON File
-# ==============================
 
 import json  # Thư viện chuẩn để xử lý JSON
 
-# 🔹 Python Object (dict)
+# Python Object (dict)
 pythonObject = {
     "ten": "Trần Duy Thanh",
     "tuoi": 50,
     "ma": "nv1"
 }
 
-# 🔹 Chuyển Python Object -> JSON String
+# Chuyển Python Object -> JSON String
 json_str = json.dumps(pythonObject, ensure_ascii=False)
 
-# 🔹 Xuất kết quả
+# Xuất kết quả
 print("Dạng JSON String:", json_str)
 print("Kiểu dữ liệu:", type(json_str))
